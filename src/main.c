db475f1c439a349baf7c41bc5eeb217e66082106
artemisfowl <skb655952@gmail.com>
2018-12-15 17:26:48 +0530

    Added the project header in place of utility

--- src/main.c
+++ src/main.c
@@ -4,10 +4,32 @@
  * @brief mkproject program main function
  */
 
-#include "../inc/utility.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "../inc/project.h"
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	printf("Working!!!\n");
+	if (argc < MIN_ARGS || argc > MAX_ARGS) {
+		printf("Error in number of arguments\n");
+		p_display_usage();
+		exit(0);
+	}
+
+	/* decrement the arg count so that we do not handle the name of the
+	 * program and increment the arg value pointer to point to the
+	 * first argument instead of the name of the program in argv[0] */
+	argc--;
+	argv++;
+
+	/* create an object/instance of the structure */
+
+	/* initialize the program */
+
+	/* count till argc drops */
+	while (argc--) {
+		/* call the function to parse the flags */
+	}
+
 	return 0;
 }
