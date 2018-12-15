f1a813668c62a400e60eb879d6d16133d105b906
artemisfowl <skb655952@gmail.com>
2018-12-15 17:56:27 +0530

    Added the project struct details

--- inc/project.h
+++ inc/project.h
@@ -13,5 +13,26 @@
 #define MIN_ARGS 2
 #endif
 
+/* enum */
+enum project_t {	/* this might be deprecated later */
+	C,
+	CPP,
+	nentries = 2
+};
+
+/* structure */
+struct project {
+	enum project_t ptype;
+	int rdp_t;
+
+	/* directory holding fields */
+	char *cwd;
+	char *prwd;
+	char *src;
+	char *inc;
+	int nfiles;	/* number of files to be copied */
+};
+
 /* functions */
 void p_display_usage(void);
+void p_setup_struct(struct project * restrict p);
